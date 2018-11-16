e3082da31379fc640f78dec4a149105339883f99
PLBasanta <plopez@2ionline.com>
2018-11-16 16:40:13 +0100

    initial commit

---
+++ KC-JS-001.js
@@ -0,0 +1,6 @@
+
+function calcularArea(){
+    var radioCirculo = document.getElementById("radio").value;
+    document.getElementById("resultado").value= radioCirculo*2;
+    alert(radioCirculo);
+}
