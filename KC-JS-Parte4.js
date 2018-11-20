0bc4c7eea4cf08c6ff809f9c52cafbe344e9b715
Pablo López Basanta <basanta79@gmail.com>
2018-11-20 17:28:50 +0100

    Add files via upload
    commit exercice 23

--- KC-JS-Parte4.js
+++ KC-JS-Parte4.js
@@ -47,3 +47,64 @@ function addNaturalNumbers(){
     console.log(result);
     return document.getElementById("KC_20_Suma").value = result;
 }
+
+function prodTable(){
+	let numero = document.getElementById("KC_21_tabla").value;
+	let tabla = new Array;
+	let result="<br>";
+	for (let iterator =1; iterator<=10; iterator++){
+			tabla.push(`${iterator} x ${numero} = ${iterator*numero}`);
+	}
+	tabla.forEach(item =>{
+		console.log(item);
+		result = result + item + "<br>";
+	});
+	return document.getElementById("KC_EJ21_label").innerHTML = result;
+	
+}
+
+let people = new Array;
+function storePeople(){	
+	let name = document.getElementById("KC_22_name").value;
+	let age =  document.getElementById("KC_22_age").value;
+	people.push({'nombre': name, 'edad':age});	
+}
+
+function findAdults(){
+	console.log(people);
+	let result= "";
+	if (people.length==10){
+		people.forEach( item => {
+			if (item ['edad']>17){
+				result = result + "," + item['nombre'];
+			}
+		});
+		return document.getElementById("KC_EJ22_label").innerHTML = result;
+	}else{
+		return document.getElementById("KC_EJ22_label").innerHTML = "Debe haber 10 personas";
+	}
+}
+
+let scores = new Array;
+function storeScores(){
+	let data = document.getElementById("KC_23_input").value;
+	let result = "";
+	let mean =0;
+	if (data==="terminar"){
+		scores.forEach( item => {
+			mean=0;
+			item['scores'].forEach(item => {
+				mean = mean + parseInt(item);
+			});
+			mean = mean / item['scores'].length;
+			result = result + "<br>" +item['name'] + ":" + mean;
+		});
+		return document.getElementById("KC_EJ23_label").innerHTML = result;
+		console.log(result);
+	}else{
+		let dataSplit = data.split(' ');
+		let scoreSplit= dataSplit[1].split(',');
+		scores.push({'name': dataSplit[0], 'scores':scoreSplit});
+		console.log(scores);
+	}
+}
