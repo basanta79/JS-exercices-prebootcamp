d7a069632beca68cbcd5ddc614fbacf82f3756ab
Pablo L Basanta <basanta79@gmail.com>
2018-11-21 00:07:06 +0100

    commit ej 27

---
+++ KC-JS-Parte5.js
@@ -0,0 +1,43 @@
+function pintar_fila(){
+    let tr = document.createElement('tr');
+    tr.appendChild(document.createElement('td'));
+    document.getElementById("table_KC_EJ26").appendChild(tr);
+}
+
+function createRows(){
+    let numFiles=parseInt(document.getElementById("KC_26_input").value);
+    let table = document.createElement('table')
+    table.id='table_KC_EJ26';
+    document.getElementById("KC_EJ26_label").appendChild(table);
+    for (let i=0; i<numFiles; i++){
+        pintar_fila();
+    }
+}
+
+function es_palindromo(texto){
+    let result = true;
+    texto.replace(/ /gi,'s');
+    console.log(texto);
+    let arrText = texto.split('');
+    let arrTextReverse = texto.split('');
+    arrTextReverse.reverse();
+    console.log(arrText);
+    console.log(arrTextReverse);
+    for(let i=0; i<arrText.length; i++){
+        console.log(arrText[i] + "-" + arrTextReverse[i]);
+        if (arrText[i]!==arrTextReverse[i]){
+            result=false;
+        }
+    }
+    return result;
+}
+
+function isPalindrome(){
+    let text=document.getElementById("KC_27_input").value;
+    let resultado = es_palindromo(text);
+    if (resultado) {
+        return document.getElementById("KC_EJ27_label").innerHTML = 'El texto ingresado ES palindromo';
+    }else{
+        return document.getElementById("KC_EJ27_label").innerHTML = 'El texto ingresado NO ES palindromo';
+    }
+}
